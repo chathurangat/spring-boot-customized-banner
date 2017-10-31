9b391bc2e0fc845f86e84e1e232b17956cca658f
Chathuranga Tennakoon <chathuranga@cyrup.sg>
2017-11-01 01:25:01 +0530

    first commit

---
+++ src/main/java/com/springbootdev/samples/springbootcustomizedbanner/SpringBootCustomizedBannerApplication.java
@@ -0,0 +1,12 @@
+package com.springbootdev.samples.springbootcustomizedbanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomizedBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCustomizedBannerApplication.class, args);
+	}
+}
